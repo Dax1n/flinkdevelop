92b5201d9d0c2811b7af81fc4633ff52a5f461ee
Dax1n <568275045@qq.com>
2017-04-16 20:53:30 +0800

    flink学习程序： 	1：flink wordcount 	2：flink Table Api尝鲜

---
+++ flinkwordcount/src/main/scala/com/daxin/App.scala
@@ -0,0 +1,24 @@
+package com.daxin
+
+
+import org.apache.flink.api.scala._
+/**
+ * @author ${user.name}
+ */
+object App {
+  
+  def main(args : Array[String]) {
+
+    val env = ExecutionEnvironment.getExecutionEnvironment
+    val lines = env.readTextFile("hdfs://node:9000/word/hadoop1.txt")
+    val wc= lines.flatMap(x=>x.split(" ")).map((_,1)).groupBy(0).sum(1)
+
+
+
+    wc.writeAsText("hdfs://node:9000/flink/wordcount/")
+
+    env.execute("flinkWordCount")
+
+  }
+
+}
